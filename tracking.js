1fd92a0212651a246b534b1ea7481089d478f760
Thanh Dung <87826003+yuuyung99@users.noreply.github.com>
2026-07-29 11:34:30 +0700

    Add files via upload

--- tracking.js
+++ tracking.js
@@ -1,1 +1,70 @@
 // JavaScript source code
+/*=====================================================
+    TRACKING SYSTEM
+=====================================================*/
+
+window.dataLayer = window.dataLayer || [];
+
+
+/*=====================================================
+    TRACK EVENT
+=====================================================*/
+
+function trackEvent(eventName, data = {}) {
+
+    window.dataLayer.push({
+
+        event: eventName,
+
+        ...data
+
+    });
+
+}
+
+
+/*=====================================================
+    FORM SUBMIT
+=====================================================*/
+
+function trackFormSubmit() {
+
+    trackEvent('form_submit');
+
+}
+
+
+/*=====================================================
+    CTA CLICK
+=====================================================*/
+
+function trackCTAClick(buttonName = '') {
+
+    trackEvent('cta_click', {
+
+        button_name: buttonName
+
+    });
+
+}
+
+/*=====================================================
+    PHONE CLICK
+=====================================================*/
+
+function trackPhoneClick() {
+
+    trackEvent('phone_click');
+
+}
+
+
+/*=====================================================
+    ZALO CLICK
+=====================================================*/
+
+function trackZaloClick() {
+
+    trackEvent('zalo_click');
+
+}
